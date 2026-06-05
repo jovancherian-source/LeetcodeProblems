e81e852944c2da413742922e4dd52b4a92a4b8a7
Jovan Cherian <jovancherian@gmail.com>
2026-06-05 17:06:42 +0400

    during exam commit

---
+++ duplicate.py
@@ -0,0 +1,16 @@
+class Solution():
+    def containsDuplicate(self, nums):
+        dups = set()
+        final_output =[]
+        for i in nums:
+            if i in dups:
+                final_output.append(i)
+            else:      
+                dups.add(i)
+            if len(final_output) > 0:
+                return True
+        return False
+
+
+solution = Solution()
+print(solution.containsDuplicate([1,3]))
